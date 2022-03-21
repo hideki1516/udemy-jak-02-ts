5337f1e6ce59d53ad05dc518dd63fe2899b45460
hideki <hideki.1516.web@gmail.com>
2022-03-21 09:16:30 +0900

    47-Practice2

---
+++ src/practices/Practice2.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Practice2 = () => {
+  // 明示的に返却値の型を指定することでチーム開発でのエラーを防ぐ
+  const getTotalFee = (num: number): number => {
+    const total = num * 1.1;
+    return total;
+  };
+
+  const onClickPracticeA = () => {
+    console.log(getTotalFee(1000));
+  };
+
+  return (
+    <div>
+      <p>練習問題：返却値の型指定</p>
+      <button onClick={onClickPracticeA}>練習問題2を実行</button>
+      {/* <button onClick={onClickPracticeB}>練習問題2-1を実行</button> */}
+    </div>
+  )
+};
