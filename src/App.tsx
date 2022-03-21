811a9a35ef1df92ec2f28d2d4b0295f71d8e5c92
hideki <hideki.1516.web@gmail.com>
2022-03-21 19:18:30 +0900

    State-Data-TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Todo } from './Todo';
+
+// 取得データの型定義
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
+  // stateの型定義
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos').then((res) => {
-      console.log(res);
+    axios.get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos').then((res) => {
+      setTodos(res.data);
     }).catch((err) => console.log(err))
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userId={todo.userId}  />
+      ))}
     </div>
   );
 }
